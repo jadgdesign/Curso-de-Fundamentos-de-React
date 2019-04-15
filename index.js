2a03a77f7f067d65742fd5b78af1d71d8e5b141c
Jadg Design <jadg.design@gmail.com>
2019-04-15 18:47:14 -0500

    React Hooks

--- index.js
+++ index.js
@@ -1,41 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import { render } from 'react-dom';
 import Hello from './Hello';
 import './style.css';
 
-class Blog extends Component {
-  constructor(props) {
-    super(props);
+function Contador(props) {
+  // this.state = { contador : 0}
+  const [contador, setContador] = useState(0);
 
-    this.state = {
-      articles: []
-    }
-  }
-
-  componentDidMount() {
-    let promesa = fetch('https://jsonplaceholder.typicode.com/posts');
-
-    promesa.then(response => response.json()).then(data => {
-      this.setState({
-        articles: data
-      })
-    })
-
-  }
-
-  render() {
-    return (
-      <div>
-        {
-          this.state.articles.map((article) => {
-            return <div className="card" style={ { backgroundColor: 'gray', color: 'white' } }><p>{ article.title }</p></div>
-          })
-        }
-      </div>
-    )
-  }
+  return (
+    <div>
+      <p>Conteo: { contador }</p>
+      <button onClick={ () => setContador(contador + 1 ) }>Aumentar</button>
+    </div>
+  )
 }
-
 
 class App extends Component {
   constructor() {
