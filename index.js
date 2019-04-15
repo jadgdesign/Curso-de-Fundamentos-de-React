25d7dcf035e14d6bf23031683c4149676d9fa197
jadgdesign <jadg.design@gmail.com>
2019-04-15 17:44:36 -0500

    Colecciones

--- index.js
+++ index.js
@@ -3,64 +3,32 @@ import { render } from 'react-dom';
 import Hello from './Hello';
 import './style.css';
 
-class Formulario extends Component {
+class Blog extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      email: '',
-      password: ''
+      articles: [
+        'Mi primer componenete en React',
+        'Introducción a React',
+        'Qué es React'
+      ]
     }
-  }
-
-  syncEmailChanges(email) {
-    this.setState({
-      email: email
-    })
-  }
-
-  syncPasswordChanges(password) {    
-    this.setState({
-      password: password
-    })
-  }
-
-  syncChanges(value, property) {
-    let state = {};
-
-    state[property] = value;
-    this.setState(state);
-  }
-
-  submitForm = () => {
-    console.log(this.state)
   }
 
   render() {
     return (
-      <form>
-        <input 
-          // onChange={ (ev) => { this.syncChanges(ev.target.value, 'email') } }
-          onChange={ (ev) => { this.syncEmailChanges(ev.target.value) } }
-          type="email" 
-          value={this.state.email} 
-          placeholder="Email" />
-        <input 
-          // onChange={ (ev) => { this.syncChanges(ev.target.value, 'password') } }
-          onChange={ (ev) => { this.syncPasswordChanges(ev.target.value) } }
-          type="password" 
-          value={this.state.password} 
-          placeholder="******" />
-        <div>
-          <input 
-            onClick={ this.submitForm }
-            type="submit" 
-            value="Iniciar sesión" />
-        </div>
-      </form>
+      <div>
+        {
+          this.state.articles.map((title) => {
+            return <p>{ title }</p>
+          })
+        }
+      </div>
     )
   }
 }
+
 
 class App extends Component {
   constructor() {
@@ -74,7 +42,7 @@ class App extends Component {
     let nombre = "José"
     return (
       <div>
-        <Formulario />
+        <Blog />
       </div>
     );
   }
