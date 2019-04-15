17ece691e1b919b7ee389e797da259f836c1044d
jadgdesign <jadg.design@gmail.com>
2019-04-15 16:50:23 -0500

    Mi Primer Componenete

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ import { render } from 'react-dom';
 import Hello from './Hello';
 import './style.css';
 
+function MiComponente() {
+  return <p>Hola Mundo</p>;
+}
+
+class MiComponenteDeClase extends Component {
+  render() {
+    return <p>Hola soy de la clase</p>;
+  }
+}
+
 class App extends Component {
   constructor() {
     super();
@@ -14,10 +24,8 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Hello name={this.state.name} />
-        <p>
-          Start editing to see some magic happen :)
-        </p>
+        <MiComponente />
+        <MiComponenteDeClase />
       </div>
     );
   }
